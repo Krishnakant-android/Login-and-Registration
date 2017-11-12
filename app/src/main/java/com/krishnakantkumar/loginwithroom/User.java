b37f3dfd1b607c36feeabd4f1d0961266abe18fe
Krishnakant-android <32206484+Krishnakant-android@users.noreply.github.com>
2017-11-12 15:50:29 +0530

    Login with Room Database room:compiler

---
+++ app/src/main/java/com/krishnakantkumar/loginwithroom/User.java
@@ -0,0 +1,51 @@
+package com.krishnakantkumar.loginwithroom;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "name")
+    private String name;
+    @ColumnInfo(name = "email")
+    private String email;
+    @ColumnInfo(name = "password")
+    private String password;
+
+
+    public int getId() {
+        return id;
+    }
+ 
+    public void setId(int id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/krishnakantkumar/loginwithroom/UserDao.java
@@ -0,0 +1,31 @@
+package com.krishnakantkumar.loginwithroom;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM user")
+    List<User> getAll();
+
+    @Query("SELECT * FROM user where email LIKE  :email AND password LIKE :password")
+    User findByName(String email, String password);
+
+
+    @Query("SELECT * FROM user where email LIKE  :email")
+    User findByEmail(String email);
+
+    @Query("SELECT COUNT(*) from user")
+    int countUsers();
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+}
